dd6f0fb888e762d8efcb67c0bb8ce22b7b989ef6
zhaozimei <1140173180@qq.com>
2016-05-29 21:35:59 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -2,7 +2,17 @@
 
 function double_to_one(collection) {
 
-  //在这里写入代码
+   var arr = [];
+	   for(var i=0; i<collection.length; i++){
+	  	  if(collection[i].length>0){
+			     for(var j=0; j<collection[i].length; j++){
+				      arr.push(collection[i][j])
+			      }
+		     }else{
+			      arr.push(collection[i])
+	     	}
+  	 }
+  return arr
 }
 
 module.exports = double_to_one;
